18a072ef893eca8443c919784758c21daf3b09ef
rohitgh2024ju <rohitstudying2004@gmail.com>
2026-04-26 21:24:26 +0530

    small changes

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { userModel, eventModel, teamModel, regModel } from '../models/model.js';
 import { adminMiddleware } from '../middlewares/adminMiddleware.js';
